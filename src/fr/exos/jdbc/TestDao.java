622a34ead243746f1a1babb9eb4d3489483f6733
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-16 11:45:14 +0100

    ajout d'une classe de test pour le dao

---
+++ src/fr/exos/jdbc/TestDao.java
@@ -0,0 +1,30 @@
+package fr.exos.jdbc;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class TestDao {
+	public static void main(String[] args) {
+        ArticleDao dao = new ArticleDao();
+        
+        try {
+        	// create
+        	Article newArticle = new Article (0,"Laptop", "DELL", 899.99);
+        	dao.create(newArticle);
+        	
+        	//read all
+        	List<Article> articles = dao.findAll();
+            articles.forEach(a -> System.out.println(a.getId() + " - " + a.getDescription()));
+            
+            //update
+            newArticle.setPrice(799.99);
+            dao.update(newArticle);
+            
+            //delete
+            dao.delete(newArticle.getId());
+        } catch (SQLException e) {
+        	e.printStackTrace();
+        }
+
+	}
+}
